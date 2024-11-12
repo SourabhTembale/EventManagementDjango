7b20cfbd8abed0afc9762f32ec7ab070ea81aaee
Sourabh <SOurab@gmail.com>
2024-11-12 09:47:56 +0530

    Deploy Setting

--- event_manager/settings.py
+++ event_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-9=j2t=&9xl(v+14z+v#b0pfrwwfp=t0p29@82agg53koo3p8pq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.versal.app','.now.sh']
 
 
 # Application definition
@@ -125,3 +125,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import os 
+STATICFILES_DIRS = os.path(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR,'statifiles_build','static')
