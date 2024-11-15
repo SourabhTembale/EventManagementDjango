89562ac005affa4084f7b9ec397768091606f3a6
SourabhTembale <107642036+SourabhTembale@users.noreply.github.com>
2024-11-15 13:46:39 +0530

    Update settings.py

--- event_manager/settings.py
+++ event_manager/settings.py
@@ -126,6 +126,3 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-import os 
-STATICFILES_DIRS = os.path(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR,'statifiles_build','static')
